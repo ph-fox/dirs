3b11362fd016e499ee413ed388600801f126b105
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-06-09 00:29:09 +0800

    Update dir3.py

--- dir3.py
+++ dir3.py
@@ -1,8 +1,8 @@
-import requests, threading, time, random, optparse, sys, random
+import requests, optparse, sys, os
 from colorama import Fore, Style
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 parser = optparse.OptionParser()
 parser.add_option('-u','--url',dest='url',help='target url')
 parser.add_option('-w','--wordlist',dest='wordlist',help='wordlist path')
@@ -10,45 +10,61 @@ parser.add_option('-w','--wordlist',dest='wordlist',help='wordlist path')
 url = value.url
 wordlist = value.wordlist
 
-if url is None:
-	print("""
-usage : python3 dir3.py -u <url> -w <wordlist>
-	-u : Target url
-	-w : wordlist path""")
-
-elif wordlist is None:
-	wordlist = '/usr/share/wordlists/dirb/common.txt'
-
-class dirsearch:
+class Dirsearch:
 	def __init__(self, url, wlist):
 		self.url = url
 		self.wlist = wordlist
 
+	def banner(self):
+		print("""
+|=====================================|
+| D҈1尺乇匚ㄒㄖ尺ㄚ 丂乇卂尺匚卄乇尺   |
+|-------------------------------------|
+|By: Anikin Luke | ǝʞnl uᴉʞᴉuɐ         |
+|                                     |
+|This Tool Is From:                   |
+| └─https://github.com/abalesluke     |
+|                                     |
+|=====================================|""")
+
+
 	def dirs(self):
+		ds.banner()
 		wordlists = open(self.wlist).read().splitlines()
 		for word in wordlists:
 			site = f'{self.url}/{word}'
-			r = requests.get(site)
-			code = r.status_code
-			spacer = random.choices(' ',k=len(site))
-			sys.stdout.write(f"\r{''.join(spacer)}            ")
-			sys.stdout.write(f'\r{Style.BRIGHT}{Fore.YELLOW}{site}')
-
-			if code == 404:
-				pass
-			elif code == 501:
-				pass
-			else:
-				if code == 200:
-					print(f'\r{Fore.CYAN}[{Fore.GREEN}{code}{Fore.CYAN}]{Fore.YELLOW} {site}')
+			try:
+				r = requests.get(site, verify=False)
+				code = r.status_code
+				spacer = ' '*len(site)
+				sys.stdout.write(f"\r{spacer}            ")
+				sys.stdout.write(f'\r{Style.BRIGHT}{Fore.YELLOW}{site}')
+				if code == 404:
+					pass
+				elif code == 501:
+					pass
 				else:
-					print(f'\r{Fore.CYAN}[{Fore.RED}{code}{Fore.CYAN}]{Fore.YELLOW} {site}')
+					if code == 200:
+						print(f'\r{Fore.CYAN}[{Fore.GREEN}{code}{Fore.CYAN}]{Fore.YELLOW} {site}')
+					else:
+						print(f'\r{Fore.CYAN}[{Fore.RED}{code}{Fore.CYAN}]{Fore.YELLOW} {site}')
+			except KeyboardInterrupt:
+				print('\nkeyboard KeyboardInterrupt')
+				exit(0)
+			except:
+				pass
+
+
+	def check(self):
+		if self.wlist is None:
+			self.wlist = '/usr/share/wordlists/dirb/common.txt'
+		if self.url is None:
+			ds.banner()
+			os.system(f'python3 {os.path.basename(__file__)} -h')
+			exit(0)
+		ds.dirs()
 
 
 if __name__=='__main__':
-
-	start = dirsearch(url, wordlist)
-	try:
-		start.dirs()
-	except:
-		pass
+	ds = Dirsearch(url, wordlist)
+	ds.check()

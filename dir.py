782e35e89db4ecb9a3b3851f996195b11626563d
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-06-16 18:32:50 +0800

    Update dir.py

--- dir.py
+++ dir.py
@@ -1,4 +1,4 @@
-import requests, optparse, sys, os, threading
+import requests, optparse, sys, os, threading, re
 from colorama import Fore, Style
 import urllib3
 
@@ -15,6 +15,7 @@ class Dirsearch:
 		self.url = url
 		self.wlist = wordlist
 		self.word= None
+		self.url_pat = r'http(|s)://[^.]*\.[^.]*'
 
 	def banner(self):
 		print("""
@@ -29,7 +30,7 @@ class Dirsearch:
 |=====================================|""")
 
 	def brute(self):
-		site = f'{self.url}/{self.word}'
+		site = f'{self.url}{self.word}'
 		try:
 			r = requests.get(site, verify=False)
 			code = r.status_code
@@ -48,27 +49,51 @@ class Dirsearch:
 		except KeyboardInterrupt:
 			print('\nkeyboard KeyboardInterrupt')
 			exit(0)
-		except:
+		except ConnectionResetError:
+			print('Connection Reset! err')
+		except Exception as e:
+			#print(e)
 			pass
 
 
 	def dirs(self):
-		ds.banner()
+		self.banner()
 		wordlists = open(self.wlist, encoding="ISO-8859-1").read().splitlines()
 		for word in wordlists:			
 			self.word = word
-			threading.Thread(target=ds.brute).start()
+			threading.Thread(target=self.brute).start()
+
 
 	def check(self):
 		if self.wlist is None:
-			self.wlist = '/usr/share/wordlists/dirb/common.txt'
+			wl_name = '/usr/share/wordlists/dirb/common.txt'
+			if(os.path.exists(wl_name)):
+				self.wlist = wl_name
+			elif(os.path.exists('./common.txt')):
+				self.wlist = './common.txt'
+			else:
+				wlist_url = 'https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt'
+				r = requests.get(wlist_url)
+				f = open('common.txt','w')
+				f.write(r.text)
+				self.wlist = './common.txt'
+
 		if self.url is None:
-			ds.banner()
+			self.banner()
 			os.system(f'python3 {os.path.basename(__file__)} -h')
-			exit(0)
-		ds.dirs()
+			os._exit(0)
+		else:
+			if(self.url[-1] != '/'):
+				self.url = self.url+'/'
+
+		if(re.match(self.url_pat,url)):
+			pass
+		else:
+			self.url = f'http:{self.url}'
+		self.dirs()
 
 
 if __name__=='__main__':
+	os.system('')
 	ds = Dirsearch(url, wordlist)
 	ds.check()
